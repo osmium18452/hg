28c79401bd8279bba8c4ab1bacc6281fe751d0d5
4010 <osmium18452@gmail.com>
2022-02-25 13:03:17 +0800

    no comment

--- Postprocessor.py
+++ Postprocessor.py
@@ -6,8 +6,11 @@ class Postprocessor:
     def __init__(self, filename):
         with open(filename, "r") as f:
             self.log = []
+            self.timestamp_log=[]
             for line in f.readlines():
                 self.log.append([self.to_date(int(line.strip().split(",")[0].split(":")[1].strip())),
+                                 float(line.strip().split(",")[1].split(":")[1])])
+                self.timestamp_log.append([int(line.strip().split(",")[0].split(":")[1].strip()),
                                  float(line.strip().split(",")[1].split(":")[1])])
 
     def print_log(self):
